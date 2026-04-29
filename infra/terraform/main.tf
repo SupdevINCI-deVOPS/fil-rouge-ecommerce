90bb1ce716ebaf187c5c55eb8d1bb6832236968b
ahmed <ahmed.rosfa@hotmail.fr>
2026-04-29 11:02:01 +0200

    fix: update infra config - ec2-user, ed25519 key, firewalld disable, correct IPs

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -212,6 +212,12 @@ resource "aws_instance" "vm" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   key_name               = aws_key_pair.deployer.key_name
 
+  user_data = <<-EOF
+    #!/bin/bash
+    systemctl stop firewalld
+    systemctl disable firewalld
+  EOF
+
   root_block_device {
     volume_size = var.disk_size
     volume_type = "gp2"
